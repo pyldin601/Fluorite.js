6d40a28f64ae111dea4576e2e1542a08230db220
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-08-11 09:47:54 +0300

    Add update and remove methods to query builder

--- src/query.js
+++ src/query.js
@@ -1,33 +1,52 @@
-export default (modelClass) => class Query {
-  constructor() {
-    this.knexQuery = modelClass.knex(modelClass.table);
+import { NotFoundError } from './';
+import filter from './filter';
+
+export default class Query {
+  constructor(modelClass) {
+    this.modelClass = modelClass;
+    this.knexInstance = modelClass.knex;
+    this.knexQuery = this.knexInstance(modelClass.table);
   }
 
-  where(attributes) {
-
+  filter(attributes) {
+    filter(this.knexQuery, attributes);
+    return this;
   }
 
   query(callback) {
-
+    callback(this.knexQuery);
+    return this;
   }
 
-  fetchOne() {
+  async fetchOne() {
+    const row = await this.knexQuery.first();
 
+    if (!row) {
+      throw new NotFoundError('Entity not found');
+    }
+
+    return new this.modelClass(row);
   }
 
   fetchAll() {
-
+    return this.knexQuery
+      .select('*')
+      .then(rows => rows.map(row => new this.modelClass(row)));
   }
 
-  fetchColumn(column = null) {
-
+  pluck(column) {
+    return this.knexQuery.pluck(column);
   }
 
   count(column = null) {
-
+    return this.knexQuery.count();
   }
 
-  save(attributes) {
+  update(attributes) {
+    return this.knexQuery.update(attributes);
+  }
 
+  remove() {
+    return this.knexQuery.delete();
   }
 };
