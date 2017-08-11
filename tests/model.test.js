190c910eceb95dd8b1fd5cd831211d8106d45eaa
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-08-11 09:44:20 +0300

    Add few filter tests

--- tests/model.test.js
+++ tests/model.test.js
@@ -5,11 +5,13 @@ import { NotFoundError } from '../src';
 beforeAll(async () => {
   await knex.schema.createTable('foo', (table) => {
     table.increments();
-    table.string('name');
+    table.string('name').unsigned().notNullable();
+    table.integer('age').unsigned().notNullable()
   });
 
-  await knex('foo').insert({ name: 'John Doe' });
-  await knex('foo').insert({ name: 'Bob Marley' });
+  await knex('foo').insert({ name: 'John Doe', age: 46 });
+  await knex('foo').insert({ name: 'Bob Marley', age: 72 });
+  await knex('foo').insert({ name: 'Billy', age: 12 });
 });
 
 describe('Model tests', () => {
@@ -21,7 +23,7 @@ describe('Model tests', () => {
 
   it('Test find by id (if exists)', async () => {
     const foo1 = await Foo.find(1);
-    expect(foo1.attributes).toEqual({ id: 1, name: 'John Doe' });
+    expect(foo1.attributes).toEqual({ id: 1, name: 'John Doe', age: 46 });
     expect(foo1.id).toBe(1);
     expect(foo1.get('name')).toBe('John Doe');
   });
@@ -34,5 +36,32 @@ describe('Model tests', () => {
       expect(e).toBeInstanceOf(NotFoundError)
       expect(e.message).toBe('Entity not found');
     }
+  });
+
+  it('Test fetchAll', async () => {
+    const foos = await Foo.fetchAll();
+    expect(foos).toBeInstanceOf(Array);
+    expect(foos.length).toBe(3);
+    for (const foo of foos) {
+      expect(foo).toBeInstanceOf(Foo);
+    }
+  });
+
+  it('Test serialization', async () => {
+    const foos = await Foo.fetchAll();
+    const json = JSON.stringify(foos);
+    expect(json).toBe(
+      '[{"id":1,"name":"John Doe","age":46},{"id":2,"name":"Bob Marley","age":72},{"id":3,"name":"Billy","age":12}]',
+    );
+  });
+
+  it('Test filter "eq"', async () => {
+    const foos = await Foo.filter({ 'age__eq': 12 }).fetchAll();
+    expect(foos.length).toBe(1);
+  });
+
+  it('Test filter "gt"', async () => {
+    const foos = await Foo.filter({ 'age__gt': 18 }).fetchAll();
+    expect(foos.length).toBe(2);
   });
 });
